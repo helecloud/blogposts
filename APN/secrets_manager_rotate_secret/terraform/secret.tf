c4aa434f9923725fa653e1e1c50ff43799b466df
Nikolay Bunev <nikolayb@helecloud.com>
2020-06-19 16:05:20 +0300

    add terraform code for db secret rotation

---
+++ APN/secrets_manager_rotate_secret/terraform/secret.tf
@@ -0,0 +1,11 @@
+resource "aws_secretsmanager_secret" "db-secret-rotation" {
+  name        = "db-secret-app01"
+  description = "This is an example secret"
+  kms_key_id  = aws_kms_key.kms_key_db_secret.arn
+  policy      = data.template_file.db_secret_policy.rendered
+
+  rotation_lambda_arn = aws_lambda_function.db_autorotation.arn
+  rotation_rules {
+    automatically_after_days = 90
+  }
+}
